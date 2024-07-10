339dc647737d13aa6746cf129eb5b8672003b2ae
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-07-10 12:30:01 +0430

    text, view, image, scrollview and imagebackground coverd

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,26 @@
-import { Text, View } from "react-native";
-
+import { Text, View, Image,ScrollView, ImageBackground} from "react-native";
+const logImage = require("../assets/images/react-logo.png");
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{flex: 1,padding:50}}>
+      <ScrollView>
+      {/* <ImageBackground source={logImage} style={{flex:1}}> */}
+      <Image source={logImage}/>
+      <Text>Hello Esmatullah</Text>
       <Text>
-        Hello Esmatullah
+      In publishing and graphic design, 
+      Lorem ipsum is a placeholder text 
+      commonly used to demonstrate the visual 
+      form of a document or a typeface without relying 
+      on meaningful content. Lorem ipsum may be used as a 
+      placeholder before the final copy is available.
       </Text>
+      <Image source={{uri:'https://picsum.photos/200/300'}} style={{width:300,height:300}}/>
+      <Image source={{uri:'https://picsum.photos/200/300'}} style={{width:300,height:300}}/>
+      <Image source={{uri:'https://picsum.photos/200/300'}} style={{width:300,height:300}}/>
+      {/* </ImageBackground> */}
+      {/* <Image source={{uri:'online image from the web'}} style={{width:300,height:300}}/> */}
+        </ScrollView>
     </View>
   );
 }
